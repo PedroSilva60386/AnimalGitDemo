44151927f430a07b1bfcd5212961251bb5714f6c
Pedro <pmlu.silva@campus.fct.unl.pt>
2022-09-17 13:30:59 +0100

    Initial version

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ *  @author Pedro Silva nº60386
+ *          Andre Lopes nº 60000
+ *          Joao Pedro Pereira nº 60974
+ *
+ */
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Animal git demo");
+    }
+}
